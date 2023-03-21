1ea9e94ec3e2bcbe39729e8a8bb036fe64b56f93
VecTorry <joshuavictor19@yahoo.com>
2023-03-21 15:33:44 +0800

    2nd commit test

--- indexActivity.js
+++ indexActivity.js
@@ -1,2 +1,13 @@
 console.log("Hello, Welcome to De La Salle University!");
-console.log("testing");
+
+let addr = ["test", "testing"]
+
+var userdetails {
+	firstName: "Ryan",
+	lastName: "Reynolds",
+	age: "12",
+	hobbies: ["wrestling", "airsoft", "hiking", "cooking", "boxing"],
+	homeAddress: addr
+}
+
+console.log(userdetails);
